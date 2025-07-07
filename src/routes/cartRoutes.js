3b4829d84173fb0775ccb4ea95935d9e60763844
RenatoAlvR <alvarezra.renato@gmail.com>
2025-07-07 02:53:51 -0400

    Proteger rutas con auth

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/cartController');
+const { protect } = require('../middleware/auth');
 
-router.post('/add', cartController.addToCart);
-router.delete('/remove', cartController.removeFromCart);
-router.patch('/update', cartController.updateCartItem);
-router.delete('/empty/:userId', cartController.emptyCart);
-router.get('/:userId', cartController.getCartByUser);
+router.post('/add', protect, cartController.addToCart);
+router.delete('/remove', protect, cartController.removeFromCart);
+router.patch('/update', protect,  cartController.updateCartItem);
+router.delete('/empty/:userId', protect, cartController.emptyCart);
+router.get('/:userId', protect,  cartController.getCartByUser);
 
 module.exports = router;
--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -2,10 +2,11 @@ const Product = require('../models/Product');
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
+const { protect } = require('../middleware/auth');
 
-router.get('/', orderController.getAllOrders);
-router.get('/:id', orderController.getOrderById);
-router.post('/', orderController.createOrder);
-router.delete('/:id', orderController.deleteOrder);
+router.get('/', protect, orderController.getAllOrders);
+router.get('/:id', protect, orderController.getOrderById);
+router.post('/', protect, orderController.createOrder);
+router.delete('/:id', protect, orderController.deleteOrder);
 
 module.exports = router;
